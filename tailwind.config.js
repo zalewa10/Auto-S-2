acfad85e8c7d087269928231542ff631e4d6141f
Krzysztof Zal <pilkarz4a@gmail.com>
2023-08-12 23:48:16 +0200

    js2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.{html,js}","./node_modules/flowbite/**/*.js"],
+  content: ["./*.{html,js}"],
+  content: ["./node_modules/flowbite/**/*.js"],
   theme: {
     extend: {},
   },
